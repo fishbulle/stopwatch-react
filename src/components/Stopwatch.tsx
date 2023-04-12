0ae285af42de59d01c9bf306acbb5fc233bc8941
Angelina <linaemalmros@gmail.com>
2023-04-12 14:30:21 +0200

    allt klart förutom att listan med sparade tider bör uppdateras automatiskt

--- src/components/Stopwatch.tsx
+++ src/components/Stopwatch.tsx
@@ -2,13 +2,15 @@ import { Box, Button, Center, Stack, Text } from '@chakra-ui/react'
 import { FaPlay, FaStop, FaSave } from 'react-icons/fa'
 import { RxReset } from 'react-icons/rx'
 import { useEffect, useState } from 'react'
-import timeService, { Time } from '../services/time-service'
+import timeService from '../services/time-service'
 import useTimes from '../hooks/useTimes'
 
 const Stopwatch = () => {
     const [timer, setTimer] = useState(0)
     const [isRunning, setIsRunning] = useState(false)
+    const [storeTime, setStoreTime] = useState(false)
     const { times, setTimes, error, setError } = useTimes()
+    const updateList = () => setStoreTime(!storeTime)
 
     useEffect(() => {
         let interval: any;
@@ -25,10 +27,16 @@ const Stopwatch = () => {
     }, [isRunning])
 
 
-    const handleSave = (time: Time) => {
+    const handleSave = (id: number, time: number) => {
+        const newSaveTime = { id, time }
+        setTimes([newSaveTime, ...times])
+
         // fetch POST saveTime
-        timeService.create(time.time)
-            .then(({ data: savedTime }) => setTimes(savedTime))
+        timeService.create({ id, time })
+            .then(({ data: savedTime }) => {
+                setTimes([savedTime, ...times])
+            })
+            .then(() => updateList())
             .catch(err => setError(err.message))
 
         setTimer(0)
@@ -37,6 +45,7 @@ const Stopwatch = () => {
     return (
         <>
             <Center>
+                {error && <Text>{error}</Text>}
                 <Box mb='50px' h='100px' w='250px' borderWidth='1px' borderRadius='lg' overflow='hidden' justifyContent='center'>
                     <Stack direction='row' spacing={0} justifyContent="center">
                         <Text fontSize='6xl'>{("0" + Math.floor((timer / 60000) % 60)).slice(-2)}:</Text>
@@ -56,7 +65,7 @@ const Stopwatch = () => {
                 <Button onClick={() => setTimer(0)} leftIcon={<RxReset />} colorScheme='pink' variant='solid'>
                     Reset
                 </Button>
-                <Button /*onClick={() => handleSave()}*/ leftIcon={<FaSave />} colorScheme='pink' variant='solid'>
+                <Button onClick={() => handleSave(0, timer)} leftIcon={<FaSave />} colorScheme='pink' variant='solid'>
                     Save
                 </Button>
             </Stack>
